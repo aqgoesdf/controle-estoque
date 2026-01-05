05f55953e3be324d894e67dc6df648d07287407b
aqgoesdf <aqgoesdf@gmail.com>
2026-01-05 18:50:13 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,68 @@
 /*AqGoEs | Dev*/
+const form = document.getElementById('formProduto');
+const erro = document.getElementById('erro');
+const tabela = document.getElementById('tabelaProdutos');
+
+let produtos = [];
+
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value.trim();
+    const quantidade = Number(document.getElementById('quantidade').value)
+
+    if(!nome || quantidade <= 0){
+        erro.textContent = 'Preencha corretamente os dados';
+        return;
+    }
+
+    erro.textContent = '';
+
+    const produto = {
+        nome: nome,
+        quantidade: quantidade
+    };
+
+    produtos.push(produto);
+    
+
+    atualizaTabela();
+    form.requestFullscreen();
+});
+
+/*Atualização da tabela*/
+function atualizaTabela() {
+    tabela.innerHTML = '';
+
+
+    produtos.forEach((produto, index) => {
+        const linha = document.getElementById('tr');
+
+        linha.innerHTML = `
+            <td>${produto.nome}</td>
+            <td>${produto.quantidade}</td>
+            <td>
+                <button class="acao" onclick="entrada(${index})">+</buuton>
+                <buuton class="acao" onclick="saida"(${index})-</button>
+            </td>
+        `;
+
+        tabela.appendChild(linha);
+    });
+}
+
+/*Função entrada de produtos*/
+function entrada(index){
+    produtos[index].quantidade++;
+    atualizaTabela();
+}
+
+/*Função saída de produtos*/
+function saida(index){
+    if(produtos[index].quantidade > 0){
+       produtos[index].quantidade--;
+       atualizaTabela();
+    }
+}
