9b309ceb8ffdf8ccebf46bb6e6fd648e213fd74f
aqgoesdf <aqgoesdf@gmail.com>
2026-01-13 18:01:58 -0300

    Cadastro de produtos.
    Criado super user, criado pasta para modelos html, criado o arquivo forms.py para o cadastros dos produtos.

--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .models import Produto
+from .forms import ProdutoFrom
+
+def lista_produtos(request):
+    if request.method == "POST":
+        form = ProdutoFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoFrom()
+
+
+    produtos = Produto.objects.all()
+    return render(request, 'estoque/lista.html',{
+        'form': form,
+        'produtos': produtos
+    })
